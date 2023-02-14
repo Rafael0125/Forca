03ef90cd33387ede5178198a6db41ce1e3f8de72
RAFAEL NAVARRO <rafanavarro0125@gmail.com>
2023-02-14 13:16:18 -0300

    atualização

--- forca/src/app/jogo/palavra/palavra.component.ts
+++ forca/src/app/jogo/palavra/palavra.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-palavra',
   templateUrl: './palavra.component.html',
   styleUrls: ['./palavra.component.css']
 })
-export class PalavraComponent {
+export class PalavraComponent implements OnInit {
+
+  public underline:string = '_'
+  public letra!:string
+
+
+
+  constructor(){ }
+
+  ngOnInit(){ }
+
+
+
+  public mudarLetra(letra:Event){ /// Lógica para o retorno da letra do botão, quando clicado
+    this.letra = (<HTMLInputElement>letra.target).innerText
+    this.underline = this.letra
+    console.log(this.letra)
+    
+  }
 
 }
